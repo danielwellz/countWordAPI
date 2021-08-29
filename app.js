4d3f679bbbd82a0bbb7bfb41d12809ea8a79ef71
Danial hendi <danial.hendi@gmailcom>
2021-08-29 11:50:59 +0430

    removing the unused changes

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const textract = require('textract');
+const lc = require('letter-count');
+
 function cfe(fileName) {
   extension = fileName.split('.').pop();
   return extension;
-  };  
+};
 const crawler = require('crawler-request');
 var options = {
   type: 'text' // extract the actual text in the pdf file
 }
 var spawn = require('child_process').spawn;
-const { stdout } = require('process');
-const { error } = require('console');
-const { log } = require('util');
-const { spawnSync } = require('child_process');
+const {
+  stdout
+} = require('process');
+const {
+  error
+} = require('console');
+const {
+  log
+} = require('util');
+const {
+  spawnSync
+} = require('child_process');
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 console.time("runtime")
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   var link = req.body.link;
 
   const extention = cfe(link);
@@ -28,13 +38,13 @@ app.post("/", function(req, res) {
 
   if (extention === "pdf") {
 
-    crawler(link).then(function(response) {
-      console.log(response);
+    crawler(link).then(function (response) {
+      
 
       let length = response.text.length;
       const enter = "\n";
 
-//----------------------------------------------------------------------------
+      //----------------------------------------------------------------------------
 
       if (response.text === enter.repeat(length)) {
         console.log("this is a scanned pdf!");
@@ -42,49 +52,53 @@ app.post("/", function(req, res) {
 
         arg1 = link;
 
-      py = spawn('python', ['pythoncode.py', arg1])
+        py = spawn('python', ['pythoncode.py', arg1])
 
-      console.log("running")
+        console.log("running")
 
-      py.stdout.on('data', (data) => {
+        py.stdout.on('data', (data) => {
 
-        // console.log(data);
+          // console.log(data);
 
-        // textract.fromBufferWithMime(txt, data, function( error, text ) {})
-        
-        // if(!error){
-        // console.log(text);}
-        // else {console.log(error);}
+          // textract.fromBufferWithMime(txt, data, function( error, text ) {})
 
-        textract.fromFileWithPath("out_text.txt", function(err, text) {
+          // if(!error){
+          // console.log(text);}
+          // else {console.log(error);}
 
-          if(!err) {
-            console.log(text);
-            res.json(wc(JSON.stringify(text)));
-          } else {
-            console.log(err);
-          }
+          textract.fromFileWithPath("out_text.txt", function (err, text) {
+
+            if (!err) {
+              console.log(text);
+              res.json({
+                words: wc(text),
+                chars: lc.count('-c', text),
+                num: lc.count('-n', text)
+              });
+            } else {
+              console.log(err);
+            }
+          });
         });
-    });
 
 
-      py.stderr.on('data', (data) => {
-        console.error(data);
-      })
+        py.stderr.on('data', (data) => {
+          console.error(data);
+        })
 
-      py.on('exit', (code) => {
-        console.log("Process quit with code : " + code);
+        py.on('exit', (code) => {
+          console.log("Process quit with code : " + code);
 
 
-    });
+        });
 
-    py.stdin.end();
 
- 
-//--------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------
 
       } else {
-        res.json(wc(response.text));
+        res.json({words: wc(response.text),
+          chars: lc.count('-c', response.text).chars,
+          num: lc.count('-n', response.text).numbers});
       }
     });
 
@@ -92,12 +106,14 @@ app.post("/", function(req, res) {
 
   } else if (extention === "docx" || extention === "pptx" || extention === "xlsx") {
 
-    textract.fromUrl(link, function(error, text) {
+    textract.fromUrl(link, function (error, text) {
       if (error) {
         console.log(error);
       } else {
 
-        res.json(wc(text));
+        res.json({ words: wc(text),
+          chars: lc.count('-c', text),
+          num: lc.count('-n', text)});
       }
     });
 
@@ -113,43 +129,40 @@ console.timeEnd("runtime")
 
 var port = process.env.PORT || 3001;
 
-app.listen(port, function() {
+app.listen(port, function () {
 
-  console.log("sever started on port " + port );
+  console.log("sever started on port " + port);
 });
 
 //----------------------------------------------------------------------------
 
 var OUT = 0;
 var IN = 1;
- 
 
-function wc( str)
-{
-    var state = OUT;
-    var wc = 0; 
-    var i = 0;
-     
-    
-    while (i < str.length)
-    {
-     
-       
-        if (str[i] == ' ' || str[i] == '\n'||
-                              str[i] == '\t')
-            state = OUT;
-             
- 
-    
-        else if (state == OUT)
-        {
-            state = IN;
-            ++wc;
-        }
- 
-   
-        ++i;
+
+function wc(str) {
+  var state = OUT;
+  var wc = 0;
+  var i = 0;
+
+
+  while (i < str.length) {
+
+
+    if (str[i] == ' ' || str[i] == '\n' ||
+      str[i] == '\t')
+      state = OUT;
+
+
+
+    else if (state == OUT) {
+      state = IN;
+      ++wc;
     }
-     
-    return "total number of words : " + wc;
+
+
+    ++i;
+  }
+
+  return "total number of words : " + wc;
 }
